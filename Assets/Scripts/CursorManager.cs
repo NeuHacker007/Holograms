f5f269a0049c8796bdd77f48548f5e656955c07b
Yifan Zhang <Zhang.yifan2@husky.neu.edu>
2017-01-30 15:44:31 -0500

    1. update gaze Manager inheritance 2. Complete cursor manager functionality

--- Assets/Scripts/CursorManager.cs
+++ Assets/Scripts/CursorManager.cs
@@ -2,88 +2,42 @@
 
 public class CursorManager : Singleton<CursorManager>
 {
-    /*
-     * 1. physical raycast
-     * 2. store the position and normal of the raycast intersection
-     * 3. If raycast doesn't hit, set position to default
-     */
-    [Tooltip("max gaze distance to calc a hit")]
-    public float MaxGazeDistance = 0.8f;
+    public GameObject CursorOnHolograms;
+    public GameObject CursorOffHolograms;
 
-    public RaycastHit HitInfo { get; private set; }
 
-    public Vector3 Position { get; private set; }
-
-    public Vector3 Normal { get; private set; }
-
-    public bool Hit { get; private set; }
-    [Tooltip("select the layers raycast should target")]
-    public LayerMask RaycastLayerMask = Physics.DefaultRaycastLayers;
-    private GazeStabilizer gazeGazeStabilizer;
-    private Vector3 gazeOrigin;
-    private Vector3 gazeDirection;
-
-    // Use this for initialization
-    void Awake()
+    void awake()
     {
-        //get gaze stabilizer
-
-        gazeGazeStabilizer = GetComponent<GazeStabilizer>();
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // assign camera position to gaze orign
-        gazeOrigin = Camera.main.transform.position;
-
-
-
-        //assign camera's main transform forward to gaze direction
-        gazeDirection = Camera.main.transform.forward;
-
-        //Using gaze Stabilizer, call function updateheadstability
-
-        gazeGazeStabilizer.UpdateHeadStability(gazeOrigin, Camera.main.transform.rotation);
-
-
-        //using gazeStabilizer get stable head position and assign it to gazeOrigin
-        gazeOrigin = gazeGazeStabilizer.StableHeadPosition;
-
-        //update raycast
-        updateRaycast();
-    }
-
-    private void updateRaycast()
-    {
-
-        // declare hitInfo
-        RaycastHit hitInfo;
-
-
-        //Perform a unity physics raycast
-        Hit = Physics.Raycast(gazeOrigin, gazeDirection, out hitInfo, MaxGazeDistance, RaycastLayerMask);
-
-        //assign hitInfo to Hitinfo public property
-        HitInfo = hitInfo;
-        //if hit, set public position = hitinfo.postion and public rmal = hitinfo.Normal
-
-        if (Hit)
+        if (CursorOnHolograms == null || CursorOffHolograms == null)
         {
-            Position = hitInfo.point;
-            Normal = hitInfo.normal;
+            return;
+        }
+
+        CursorOnHolograms.SetActive(false);
+        CursorOffHolograms.SetActive(false);
+    }
+
+    void update()
+    {
+        if (GazeManager.Instance == null || CursorOffHolograms == null || CursorOnHolograms == null)
+        {
+            return;
+        }
+
+        if (GazeManager.Instance.Hit)
+        {
+            CursorOnHolograms.SetActive(true);
+            CursorOffHolograms.SetActive(false);
         }
         else
         {
-            //set position to gazeOrigin +  (gazeDirection * MaxGazeDistance);
-
-            Position = gazeOrigin + (gazeDirection * MaxGazeDistance);
-            // assign normal to gaze position.
-            Normal = gazeDirection;
+            CursorOffHolograms.SetActive(true);
+            CursorOnHolograms.SetActive(false);
         }
 
-
-
+        gameObject.transform.position = GazeManager.Instance.Position;
+        gameObject.transform.up = GazeManager.Instance.Normal;
     }
+
+
 }
---
+++ Assets/Scripts/GazeManager.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+
+public class GazeManager : Singleton<GazeManager>
+{
+
+    /*
+        * 1. physical raycast
+        * 2. store the position and normal of the raycast intersection
+        * 3. If raycast doesn't hit, set position to default
+        */
+    [Tooltip("max gaze distance to calc a hit")]
+    public float MaxGazeDistance = 0.8f;
+
+    public RaycastHit HitInfo { get; private set; }
+
+    public Vector3 Position { get; private set; }
+
+    public Vector3 Normal { get; private set; }
+
+    public bool Hit { get; private set; }
+    [Tooltip("select the layers raycast should target")]
+    public LayerMask RaycastLayerMask = Physics.DefaultRaycastLayers;
+    private GazeStabilizer gazeGazeStabilizer;
+    private Vector3 gazeOrigin;
+    private Vector3 gazeDirection;
+
+    // Use this for initialization
+    void Awake()
+    {
+        //get gaze stabilizer
+
+        gazeGazeStabilizer = GetComponent<GazeStabilizer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // assign camera position to gaze orign
+        gazeOrigin = Camera.main.transform.position;
+
+
+
+        //assign camera's main transform forward to gaze direction
+        gazeDirection = Camera.main.transform.forward;
+
+        //Using gaze Stabilizer, call function updateheadstability
+
+        gazeGazeStabilizer.UpdateHeadStability(gazeOrigin, Camera.main.transform.rotation);
+
+
+        //using gazeStabilizer get stable head position and assign it to gazeOrigin
+        gazeOrigin = gazeGazeStabilizer.StableHeadPosition;
+
+        //update raycast
+        updateRaycast();
+    }
+
+    private void updateRaycast()
+    {
+
+        // declare hitInfo
+        RaycastHit hitInfo;
+
+
+        //Perform a unity physics raycast
+        Hit = Physics.Raycast(gazeOrigin, gazeDirection, out hitInfo, MaxGazeDistance, RaycastLayerMask);
+
+        //assign hitInfo to Hitinfo public property
+        HitInfo = hitInfo;
+        //if hit, set public position = hitinfo.postion and public rmal = hitinfo.Normal
+
+        if (Hit)
+        {
+            Position = hitInfo.point;
+            Normal = hitInfo.normal;
+        }
+        else
+        {
+            //set position to gazeOrigin +  (gazeDirection * MaxGazeDistance);
+
+            Position = gazeOrigin + (gazeDirection * MaxGazeDistance);
+            // assign normal to gaze position.
+            Normal = gazeDirection;
+        }
+
+
+
+    }
+}
