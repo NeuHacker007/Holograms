eb206b7630ddbaf14fc5527bab8b962ce0b5fa0f
Yifan Zhang <Zhang.yifan2@husky.neu.edu>
2017-01-30 09:33:13 -0500

    Complete the Cursor Manager functionalities

--- Assets/Scripts/CursorManager.cs
+++ Assets/Scripts/CursorManager.cs
@@ -2,11 +2,27 @@
 
 public class CursorManager : Singleton<CursorManager>
 {
-    public GameObject CursorOnHolograms;
-    public GameObject CursorOffHolograms;
+    /*
+     * 1. physical raycast
+     * 2. store the position and normal of the raycast intersection
+     * 3. If raycast doesn't hit, set position to default
+     */
+    public float MaxGazeDistance = 0.8f;
 
 
-    void awake()
+    public Vector3 Position { get; private set; }
+
+    public Vector3 Normal { get; private set; }
+
+    public bool Hit { get; private set; }
+
+
+    private GazeStabilizer gazeGazeStabilizer;
+    private Vector3 gazeOrigin;
+    private Vector3 gazeDirection;
+
+    // Use this for initialization
+    void Awake()
     {
         if (CursorOnHolograms == null || CursorOffHolograms == null)
         {
@@ -24,16 +40,45 @@ public class CursorManager : Singleton<CursorManager>
             return;
         }
 
-        if (GazeManager.Instance.Hit)
-        {
-            CursorOnHolograms.SetActive(true);
-            CursorOffHolograms.SetActive(false);
-        }
-        else
-        {
-            CursorOffHolograms.SetActive(true);
-            CursorOnHolograms.SetActive(false);
-        }
+    // Update is called once per frame
+    void Update()
+    {
+        // assign camera position to gaze orign
+        gazeOrigin = Camera.main.transform.position;
+
+
+
+        //assign camera's main transform forward to gaze direction
+        gazeDirection = Camera.main.transform.forward;
+
+        //Using gaze Stabilizer, call function updateheadstability
+
+        gazeGazeStabilizer.UpdateHeadStability(gazeOrigin, Camera.main.transform.rotation);
+
+        gazeOrigin = gazeGazeStabilizer.StableHeadPosition;
+
+        updateRaycast();
+
+
+        //using gazeStabilizer get stable head position and assign it to gazeOrigin
+
+
+        //update raycast
+
+
+    }
+
+    private void updateRaycast()
+    {
+
+        // declare hitInfo
+
+        //Perform a unity physics raycast
+
+
+        //assign hitInfo to Hitinfo public property
+
+        //if hit, set public position = hitinfo.postion and public Normal = hitinfo.Normal
 
         gameObject.transform.position = GazeManager.Instance.Position;
         gameObject.transform.up = GazeManager.Instance.Normal;
