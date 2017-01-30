5ef876f72ea59a52f5fad87c659fb47e79c9ae5a
Yifan Zhang <Zhang.yifan2@husky.neu.edu>
2017-01-30 09:33:13 -0500

    Complete the Cursor Manager functionalities

--- Assets/Scripts/CursorManager.cs
+++ Assets/Scripts/CursorManager.cs
@@ -7,6 +7,7 @@ public class CursorManager : Singleton<CursorManager>
      * 2. store the position and normal of the raycast intersection
      * 3. If raycast doesn't hit, set position to default
      */
+    [Tooltip("max gaze distance to calc a hit")]
     public float MaxGazeDistance = 0.8f;
 
     public RaycastHit HitInfo { get; private set; }
@@ -16,8 +17,8 @@ public class CursorManager : Singleton<CursorManager>
     public Vector3 Normal { get; private set; }
 
     public bool Hit { get; private set; }
-
-
+    [Tooltip("select the layers raycast should target")]
+    public LayerMask RaycastLayerMask = Physics.DefaultRaycastLayers;
     private GazeStabilizer gazeGazeStabilizer;
     private Vector3 gazeOrigin;
     private Vector3 gazeDirection;
@@ -46,34 +47,43 @@ public class CursorManager : Singleton<CursorManager>
 
         gazeGazeStabilizer.UpdateHeadStability(gazeOrigin, Camera.main.transform.rotation);
 
-        gazeOrigin = gazeGazeStabilizer.StableHeadPosition;
-
-        updateRaycast();
-
 
         //using gazeStabilizer get stable head position and assign it to gazeOrigin
-
+        gazeOrigin = gazeGazeStabilizer.StableHeadPosition;
 
         //update raycast
-
-
+        updateRaycast();
     }
 
     private void updateRaycast()
     {
 
         // declare hitInfo
+        RaycastHit hitInfo;
+
 
         //Perform a unity physics raycast
-
+        Hit = Physics.Raycast(gazeOrigin, gazeDirection, out hitInfo, MaxGazeDistance, RaycastLayerMask);
 
         //assign hitInfo to Hitinfo public property
+        HitInfo = hitInfo;
+        //if hit, set public position = hitinfo.postion and public rmal = hitinfo.Normal
 
-        //if hit, set public position = hitinfo.postion and public Normal = hitinfo.Normal
+        if (Hit)
+        {
+            Position = hitInfo.point;
+            Normal = hitInfo.normal;
+        }
+        else
+        {
+            //set position to gazeOrigin +  (gazeDirection * MaxGazeDistance);
+
+            Position = gazeOrigin + (gazeDirection * MaxGazeDistance);
+            // assign normal to gaze position.
+            Normal = gazeDirection;
+        }
 
 
-        // else, save defaults. set position to gazeOrigin +  (gazeDirection * MaxGazeDistance);
-        // assign normal to gaze position.
 
     }
 }
