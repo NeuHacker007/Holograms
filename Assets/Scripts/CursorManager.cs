286f351206d2a8746baec63e91a824b361afba3f
Yifan Zhang <Zhang.yifan2@husky.neu.edu>
2017-02-03 11:45:07 -0500

    updated the gaze manager control

--- Assets/Scripts/CursorManager.cs
+++ Assets/Scripts/CursorManager.cs
@@ -7,19 +7,8 @@ public class CursorManager : Singleton<CursorManager>
      * 2. store the position and normal of the raycast intersection
      * 3. If raycast doesn't hit, set position to default
      */
-    public float MaxGazeDistance = 0.8f;
-
-
-    public Vector3 Position { get; private set; }
-
-    public Vector3 Normal { get; private set; }
-
-    public bool Hit { get; private set; }
-
-
-    private GazeStabilizer gazeGazeStabilizer;
-    private Vector3 gazeOrigin;
-    private Vector3 gazeDirection;
+    public GameObject CursorOnHolograms;
+    public GameObject CursorOffHolograms;
 
     // Use this for initialization
     void Awake()
@@ -40,48 +29,20 @@ public class CursorManager : Singleton<CursorManager>
             return;
         }
 
-    // Update is called once per frame
-    void Update()
-    {
-        // assign camera position to gaze orign
-        gazeOrigin = Camera.main.transform.position;
-
-
-
-        //assign camera's main transform forward to gaze direction
-        gazeDirection = Camera.main.transform.forward;
-
-        //Using gaze Stabilizer, call function updateheadstability
-
-        gazeGazeStabilizer.UpdateHeadStability(gazeOrigin, Camera.main.transform.rotation);
-
-        gazeOrigin = gazeGazeStabilizer.StableHeadPosition;
-
-        updateRaycast();
-
-
-        //using gazeStabilizer get stable head position and assign it to gazeOrigin
-
-
-        //update raycast
-
-
-    }
-
-    private void updateRaycast()
-    {
-
-        // declare hitInfo
-
-        //Perform a unity physics raycast
-
-
-        //assign hitInfo to Hitinfo public property
-
-        //if hit, set public position = hitinfo.postion and public Normal = hitinfo.Normal
+        if (GazeManager.Instance.Hit)
+        {
+            CursorOnHolograms.SetActive(true);
+            CursorOffHolograms.SetActive(false);
+        }
+        else
+        {
+            CursorOffHolograms.SetActive(true);
+            CursorOnHolograms.SetActive(true);
+        }
 
         gameObject.transform.position = GazeManager.Instance.Position;
         gameObject.transform.up = GazeManager.Instance.Normal;
+
     }
 
 
---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class Interactable : Singleton<Interactable>
+{
+
+    public GameObject forcusedGameObject { get; private set; }
+
+    public GameObject oldFocusGameObject = null;
+    // Use this for initialization
+    void Start()
+    {
+        forcusedGameObject = null;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        oldFocusGameObject = forcusedGameObject;
+        if (GazeManager.Instance.Hit)
+        {
+            RaycastHit hitInfo = GazeManager.Instance.HitInfo;
+            if (hitInfo.collider != null)
+            {
+                forcusedGameObject = hitInfo.collider.gameObject;
+            }
+            else
+            {
+                forcusedGameObject = null;
+            }
+        }
+        else
+        {
+            forcusedGameObject = null;
+        }
+
+        if (forcusedGameObject != oldFocusGameObject)
+        {
+            if (forcusedGameObject != null)
+            {
+                if (forcusedGameObject.GetComponent<Interactable>() != null)
+                {
+                    forcusedGameObject.SendMessage("GazeEntered");
+                }
+            }
+        }
+    }
+
+    private void ResetFocuseInteractivle()
+    {
+        if (oldFocusGameObject != null)
+        {
+            if (oldFocusGameObject.GetComponent<Interactable>() != null)
+            {
+                oldFocusGameObject.SendMessage("GazeExited");
+            }
+        }
+    }
+}
