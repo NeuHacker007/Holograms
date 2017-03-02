02e2010a1faa8a21ccdbb0a0510ba85c87d3ce92
Yifan Zhang <Zhang.yifan2@husky.neu.edu>
2017-03-02 10:35:48 -0500

    1. Fixed StartCoroutine function bugs 2. Solved the scence change null game object bug.

--- Assets/Scripts/ColliderManager.cs
+++ Assets/Scripts/ColliderManager.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.UI;
+using Debug = UnityEngine.Debug;
 
 public class ColliderManager : MonoBehaviour
 {
-    private GameObject txtWindow;
-    private Text text;
+    private static GameObject txtWindow;
+    private static Text text;
     private float timercount = 0.0f;
     private int index;
     private bool isTalking = false;
@@ -20,7 +22,7 @@ public class ColliderManager : MonoBehaviour
         {
             initial();
         }
-         
+        Debug.Log("Hi");
     }
 
     void OnLevelWasLoaded(int level)
@@ -31,9 +33,7 @@ public class ColliderManager : MonoBehaviour
     void initial()
     {
         txtWindow = GameObject.Find("TextDisPlayWindow");
-        //DontDestroyOnLoad(txtWindow);
-
-        //DontDestroyOnLoad(this.gameObject);
+        DontDestroyOnLoad(txtWindow);
         text = txtWindow.GetComponent<Text>();
         timercount = TextManager.Instance.timer * 1000000.00f;
         index = 0;
@@ -49,6 +49,7 @@ public class ColliderManager : MonoBehaviour
 
         VedioPlayer.Instance.movie.Play();
         VedioPlayer.Instance.audio.Play();
+
         // here we re-acquire the game object because when the scence changes, the game object assigned
         // to textWindow has been destroyed as well as the text object. In order to solve this problem
         // we re-aquire the game object and its correspond text component. 
@@ -60,18 +61,15 @@ public class ColliderManager : MonoBehaviour
         {
             text = txtWindow.GetComponent<Text>();
         }
-
-
         txtWindow.SetActive(true);
-        Debug.Log("-----------------------------------HHHHH------------------");
-        //first line of  
+        //first line of the conversation.   
         text.text = TextManager.message[index];
         Debug.Log(text.text);
         index++;
         isTalking = true;
+
         // this will give us a timer function which the nextLines function will be called
         // after 5.0f seconds.
-        Time.timeScale = 1;
         Invoke("nextLines", 5.0f);
     }
 
@@ -79,39 +77,38 @@ public class ColliderManager : MonoBehaviour
     {
         if (isTalking)
         {
-            //GameObject guopin = GameObject.Find("guopin");
-            //guopin.AddComponent<ColliderManager>();
-            // This will iterate all the Messages that is specified from Unity Inspector
-            // or use the default settings in the TextManager class.This part, the index
-            // is start from 1 which is the sencond lines in the message Array because
-            // the first line has already displayed above. 
-            while (index < TextManager.message.Length)
-            {
-                // This is used to set intervals between two lines when they are show up
-                // The reason why we use StartCoroutine function is that we have already
-                // used the Invoke function previously. I tested to use Invoke twice but failed
-                // to do so here. Therefore, we choose to use StartCoroutine function. 
-              
-               StartCoroutine(printLines());
-             // Invoke("printLines", 5.0f);
-            }
+            // This is used to set intervals between two lines when they are show up
+            // The reason why we use StartCoroutine function is that we have already
+            // used the Invoke function previously. I tested to use Invoke twice but failed
+            // to do so here. Therefore, we choose to use StartCoroutine function. 
+            StartCoroutine("Conversation");
+
         }
     }
-   IEnumerator printLines()
+    IEnumerator Conversation()
     {
-
-
-        if (isTalking && index < TextManager.message.Length && index > 0)
+        // This will iterate all the Messages that is specified from Unity Inspector
+        // or use the default settings in the TextManager class.This part, the index
+        // is start from 1 which is the sencond lines in the message Array because
+        // the first line has already displayed above. 
+        while (index < TextManager.message.Length)
         {
-           // yield return new WaitForSeconds(5);
-            text.text = TextManager.message[index++];
 
-            Debug.Log("***********" + text.text);
-
-            Invoke("nextLines", 5.0f);
+            if (isTalking && index < TextManager.message.Length && index > 0)
+            {
+                DateTime start = DateTime.Now;
+                // this is used to pause the frame of text for 5 seconds
+                yield return new WaitForSeconds(5);
+                text.text = TextManager.message[index++];
+                Debug.Log(string.Format("time escalped: {0}", DateTime.Now - start));
+                Debug.Log(text.text);
+            }
+            else
+            {
+                yield return null;
+            }
         }
-        return TextManager.message.GetEnumerator();
-        
+
     }
 
 }
